12904a624d4dbe599bb69e608ac32aed0b69df68
kalyan <programmer143143@gmail.com>
2024-12-15 17:00:47 +0000

    added JS files for debugging

--- script.js
+++ script.js
@@ -1,28 +1,48 @@
 // Function to add a new paragraph
-document.getElementsByClassName("demo").innerHTML = "Hai This is a Tag from inner html Haiya!";
-document.alert("welcome to learning page!");
+try {
+    document.getElementsByClassName("demo")[0].innerHTML = "Hai This is a Tag from inner html Haiya!";
+    console.log("Initial message set in demo class.");
+} catch (error) {
+    console.error("Error setting initial message:", error);
+}
 
+alert("Welcome to the learning page!");
 
 document.getElementById('add-paragraph').addEventListener('click', function() {
-    const newParagraph = document.createElement('p');
-    newParagraph.textContent = 'This is a new paragraph added by JavaScript.';
-    document.getElementById('content').appendChild(newParagraph);
+    try {
+        const newParagraph = document.createElement('p');
+        newParagraph.textContent = 'This is a new paragraph added by JavaScript.';
+        document.getElementById('content').appendChild(newParagraph);
+        console.log("New paragraph added.");
+    } catch (error) {
+        console.error("Error adding paragraph:", error);
+    }
 });
 
 // Function to change the title
 document.getElementById('change-title').addEventListener('click', function() {
-    const title = document.getElementById('main-title');
-    title.textContent = 'Title Changed!';
-    title.style.color = 'red'; // Change the color of the title
+    try {
+        const title = document.getElementById('main-title');
+        title.textContent = 'Title Changed!';
+        title.style.color = 'red'; // Change the color of the title
+        console.log("Title changed to 'Title Changed!'.");
+    } catch (error) {
+        console.error("Error changing title:", error);
+    }
 });
 
 // Function to remove the last paragraph
 document.getElementById('remove-paragraph').addEventListener('click', function() {
-    const contentDiv = document.getElementById('content');
-    const paragraphs = contentDiv.getElementsByTagName('p');
-    if (paragraphs.length > 0) {
-        contentDiv.removeChild(paragraphs[paragraphs.length - 1]);
-    } else {
-        alert('No paragraphs to remove!');
+    try {
+        const contentDiv = document.getElementById('content');
+        const paragraphs = contentDiv.getElementsByTagName('p');
+        if (paragraphs.length > 0) {
+            contentDiv.removeChild(paragraphs[paragraphs.length - 1]);
+            console.log("Last paragraph removed.");
+        } else {
+            alert('No paragraphs to remove!');
+        }
+    } catch (error) {
+        console.error("Error removing paragraph:", error);
     }
 });
